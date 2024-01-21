cedc49abad0d227e754206521c0e9c20d558548f
Haya Kabishi <you@example.com>
2024-01-21 23:17:42 +0200

    first commit

--- set_git.bat
+++ set_git.bat
@@ -8,5 +8,5 @@ git init
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/HayaKabishi531/WEB_Lab1.git
+git remote add origin https://github.com/HayaKabishi531/LabWab1.git
 git push -u origin main
